52971001e862e2b6d60e910aa7bc622b1b9775f0
Jordan Bank <jordan.w.bank@gmail.com>
2025-01-16 21:52:36 -0500

    made things more dynamic

--- restaurantCalculator.js
+++ restaurantCalculator.js
@@ -1,11 +1,17 @@
+const totalBill = 163
+const tax = 0.08875
 const values = [28.25, 28.25, 30.75, 38.75]
 //values are pre-tip and tax
 const names = ["Jordan", "Dan", "Andy", "Raf"]
 //should be a name for each value so value[index] belongs to name[index]
+const tip = (totalBill - values.reduce((a, b) =>  a + b, 0) * tax)/values.reduce((a, b) =>  a + b, 0) - 1
+
+console.log(tip)
+
 
 const calculator = () => {
     for (let i = 0; i < values.length; i++){
-        let afterTaxAndTip = Math.round((values[i] + (values[i] * 0.08875) + (values[i] * 0.204920635)) * 100)/100
+        let afterTaxAndTip = Math.round((values[i] + (values[i] * tax) + (values[i] * tip)) * 100)/100
         console.log(`${names[i]}: ${afterTaxAndTip}`)
     }
 }
