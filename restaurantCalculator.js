1c5de6a95b9689a974a3702b44d662cd4b0e6272
Jordan Bank <jordan.w.bank@gmail.com>
2025-01-16 21:53:27 -0500

    forgot to remove console log

--- restaurantCalculator.js
+++ restaurantCalculator.js
@@ -6,9 +6,6 @@ const names = ["Jordan", "Dan", "Andy", "Raf"]
 //should be a name for each value so value[index] belongs to name[index]
 const tip = (totalBill - values.reduce((a, b) =>  a + b, 0) * tax)/values.reduce((a, b) =>  a + b, 0) - 1
 
-console.log(tip)
-
-
 const calculator = () => {
     for (let i = 0; i < values.length; i++){
         let afterTaxAndTip = Math.round((values[i] + (values[i] * tax) + (values[i] * tip)) * 100)/100
