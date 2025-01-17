673186beb91c0eb4399264b245a0bc3db3c877b1
Jordan Bank <jordan.w.bank@gmail.com>
2025-01-16 22:21:15 -0500

    added variables

--- restaurantCalculator.js
+++ restaurantCalculator.js
@@ -4,10 +4,11 @@ const afterTax = 137.18
 //how much you paid before tip
 const values = [28.25, 28.25, 30.75, 38.75]
 //values are pre-tip and tax
+const totalBeforeTipAndTax = values.reduce((a, b) =>  a + b, 0)
 const names = ["Jordan", "Dan", "Andy", "Raf"]
 //should be a name for each value so value[index] belongs to name[index]
-const tax = (afterTax/values.reduce((a, b) =>  a + b, 0)) - 1
-const tip = (totalBill - values.reduce((a, b) =>  a + b, 0) * tax)/values.reduce((a, b) =>  a + b, 0) - 1
+const tax = (afterTax/totalBeforeTipAndTax) - 1
+const tip = (totalBill - (afterTax - totalBeforeTipAndTax))/totalBeforeTipAndTax - 1
 
 const calculator = () => {
     for (let i = 0; i < values.length; i++){
